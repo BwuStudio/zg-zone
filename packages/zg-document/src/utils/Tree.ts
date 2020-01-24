2b8b07cd9e522d95f12ddde11b38f25bb7e0a778
bwuni <bwuni@outlool.com>
2020-01-25 00:42:33 +0800

    zg-document:utils/Tree add getValue & getChildren methods

--- packages/zg-document/src/utils/Tree.ts
+++ packages/zg-document/src/utils/Tree.ts
@@ -5,21 +5,30 @@ type TreeJson<T> = {
 } & T
 
 export default class Tree<T> implements Functor<T>{
-    private v: T
-
-    private children: Tree<T>[]
     constructor(v: T) { this.v = v; this.children = [] }
-    static prase<T>(c: TreeJson<T>) { return new Tree<T>(c) }
     static gen<T>(c: T) { return new Tree<T>(c) }
+    static prase<T>(c: TreeJson<T>) {
+
+        const s = Tree.gen<T>(Object.assign({}, c, { children: null }))
+        s.children = c.children?c.children.map(v=>Tree.prase(v)):[]
+
+        return s
+    }
+
+    private v: T
+    private children: Tree<T>[]
     get() { return this.v }
 
-    map<S>(fn:(t:T)=>S):Tree<S>{
+    map<S>(fn: (t: T) => S): Tree<S> {
         const c = Tree.gen(fn(this.v))
-        c.children = this.children.map(v=>v.map(fn))
+        c.children = this.children.map(v => v.map(fn))
         return c
     }
 
-    getChildren(){
-        return this.children.map(v=>v)
+    getChildren() {
+        return this.children.map(v => v)
+    }
+    gerValue(){
+        return this.v
     }
 }
