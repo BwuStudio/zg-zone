0d301b206cc5b28f8fa3084909726b3c15707275
BwuNi <bwuni@outlook.com>
2020-02-11 21:06:51 +0800

    2-13

---
+++ packages/zg-document/build/precache-manifest.e05d75f655ad72445231f77349c9b2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5360ad8da68e13fd584f96aa904d42b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c877333c00be55366660",
+    "url": "./static/css/2.1c7ffa40.chunk.css"
+  },
+  {
+    "revision": "4a92c4f9dc61ea8e908c",
+    "url": "./static/css/main.5d08c30c.chunk.css"
+  },
+  {
+    "revision": "c877333c00be55366660",
+    "url": "./static/js/2.0c6f46d4.chunk.js"
+  },
+  {
+    "revision": "41c81e66e5124e2a4325c1cb627e2b09",
+    "url": "./static/js/2.0c6f46d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a92c4f9dc61ea8e908c",
+    "url": "./static/js/main.19d09538.chunk.js"
+  },
+  {
+    "revision": "2de6fe3bb7dac169f685",
+    "url": "./static/js/runtime-main.ef6b95ea.js"
+  }
+]);
